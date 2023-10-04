7ecc1147106cfac6e433d65711631c6982dd37d0
TeaCookie <seaniscool22@gmail.com>
2023-10-04 14:06:10 -0600

    update with templates

--- pelicanconf.py
+++ pelicanconf.py
@@ -15,6 +15,8 @@ TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
+THEME = 'content/templates/mytheme'
+
 # Blogroll
 LINKS = (('Pelican', 'https://getpelican.com/'),
          ('Python.org', 'https://www.python.org/'),
